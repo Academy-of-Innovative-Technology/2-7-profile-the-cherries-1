64fbb6eae8558ff02b895ad1024543fac573b169
emma <egujral26@aoiths.org>
2026-01-18 20:22:22 -0500

    edited HTML and filled out JS profile

--- src/js/script.js
+++ src/js/script.js
@@ -2,32 +2,39 @@
 
 var profile = {
     bio: {
-        first: "",
-        last: "",
-        title: "",
-        about: ""
+        first: "Peter",
+        last: "Parker",
+        title: "Friendly Neighborhood Spider Man",
+        about: "He's spiderman, normal boy from queens turned hero from a spider bite. He saves the city often."
     }, 
 
     social: [
-        { facebook: " "},
-        { X: "" },
-        { youtube: "" },
-        { linkedIn: "" }
+        { facebook: "@spiderman"},
+        { X: "Spider-Man" },
+        { youtube: "N/A" },
+        { linkedIn: "N/A" }
     ],
     edu: {
-        name: "",
-        major: ""
+        name: "Midtown High",
+        major: "Science"
     },
     career: {
-        company: "",
-        title: "",
-        role: ""
+        company: "The Daily Bugle",
+        title: "Freelance Photographer",
+        role: "photographer"
     },
 
     skills: [
-        { title: "", level: 0 },
-        { title: "", level: 0 },
-        { title: "", level: 0 }
+        { title: "web-shooting", level: 100 },
+        { title: "Spidey Sense", level: 100 },
+        { title: "Fighting Skills", level: 1000 }
     ]
 };
 
+const img = document.querySelector("#profile-img");
+const nameEl = document.querySelector("#profile-name");
+const titleEl = document.querySelector("#profile-title");
+const contactEl = document.querySelector("#profile-contact");
+const socialEl = document.querySelector("#social-links");
+const aboutEl = document.querySelector("#about-text");
+const skillsEl = document.querySelector("#skills-section");
